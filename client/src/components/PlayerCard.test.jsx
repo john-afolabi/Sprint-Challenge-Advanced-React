7f248f9b94e0f5fadcd28d7a1cd1dca06b988920
John Afolabi <johnafolabi.da@gmail.com>
2019-12-06 11:19:13 +0100

    Test PlayerCard component

---
+++ client/src/components/PlayerCard.test.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import PlayerCard from "./PlayerCard";
+
+afterEach(rtl.cleanup);
+
+describe("Renders Player Card from props", () => {
+  const playercard = rtl.render(<PlayerCard name={Alex} country={United States} search={20}/>);
+  it("Should render Names", () => {
+    expect(playercard.getByText(/alex/i))
+  });   
+
+  it("Should render Country", () => {
+    expect(playercard.getByText(/united states/i))
+  });   
+
+  it("Should render Search", () => {
+    expect(playercard.getByText("20"))
+  });   
+});
